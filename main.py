7bef5bca3200217a1c1909390168ba913c5addd5
tanzeem <20bcs132@iiitdwd.ac.in>
2021-09-10 00:26:45 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+def is_leap(y):
+    if 1<=y<=10**6:
+        if (y % 4 == 0 and y % 100 != 0) or (y % 100 == 0 and y % 400 == 0):
+                    return True
+        else:
+            return False
+    else:
+        return False
+year = int(input())
+print(is_leap(year))
+
+
